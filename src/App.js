906d81a80fdc9e9d9bf44bae51597133bfab6e15
毛亚琛 <maoyachen55@gmail.com>
2020-07-30 15:25:02 +0800

    add github buttons

--- src/App.js
+++ src/App.js
@@ -9,27 +9,41 @@ function App() {
 
   return (
     <div className="App">
-      <div className="control">
-        <div className="btn-group">
-          <button onClick={() => setSize("small")}>small</button>
-          <button onClick={() => setSize("normal")}>normal</button>
-          <button onClick={() => setSize("big")}>big</button>
+      <div className="container">
+        <h1 className="title">
+          Playing Cards Galary
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=clysto&repo=react-poker-components&type=star&count=true&size=large"
+            frameborder="0"
+            scrolling="0"
+            width="170"
+            height="30"
+            title="GitHub"
+            className="gh-btn"
+          ></iframe>
+        </h1>
+        <div className="control">
+          <div className="btn-group">
+            <button onClick={() => setSize("small")}>small</button>
+            <button onClick={() => setSize("normal")}>normal</button>
+            <button onClick={() => setSize("big")}>big</button>
+          </div>
         </div>
-      </div>
-      <div>
-        {ALL_POINTS.map((point) => {
-          return ALL_SUITS.map((suit) => {
-            return (
-              <PlayingCard
-                size={size}
-                suit={suit}
-                point={point}
-                key={suit + point}
-                className="test-card"
-              ></PlayingCard>
-            );
-          });
-        })}
+        <div>
+          {ALL_POINTS.map((point) => {
+            return ALL_SUITS.map((suit) => {
+              return (
+                <PlayingCard
+                  size={size}
+                  suit={suit}
+                  point={point}
+                  key={suit + point}
+                  className="test-card"
+                ></PlayingCard>
+              );
+            });
+          })}
+        </div>
       </div>
     </div>
   );
